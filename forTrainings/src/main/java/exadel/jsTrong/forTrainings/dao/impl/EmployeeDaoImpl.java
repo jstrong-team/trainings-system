9750f4ac6ca9116ec5ff5fc6c4ba296a5b5782be
Stas777 <stas@MacBook-Air-Stas.local>
2015-07-09 05:45:18 +0300

    Added EmployeeDAO, TrainingDAO. Implemented some methods. Updated ConnectionManager. Made some fixes.
    Added EmployeeDAO, TrainingDAO, corresponding Impl methods, updated
    ConnectionManager, made some fixes.

--- forTrainings/src/main/java/exadel/jsTrong/forTrainings/dao/impl/EmployeeDaoImpl.java
+++ forTrainings/src/main/java/exadel/jsTrong/forTrainings/dao/impl/EmployeeDaoImpl.java
@@ -1,5 +1,7 @@
-package exadel.jsTrong.forTrainings.dao;
+package exadel.jsTrong.forTrainings.dao.impl;
 
+import exadel.jsTrong.forTrainings.dao.EmployeeDAO;
+import exadel.jsTrong.forTrainings.dao.GenericDAO;
 import exadel.jsTrong.forTrainings.model.Employee;
 import exadel.jsTrong.forTrainings.db.ConnectionManager;
 import org.apache.log4j.Logger;
@@ -8,13 +10,14 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
-public class EmployeeDaoImpl implements DaoGeneric<Employee> {
+public class EmployeeDAOImpl implements EmployeeDAO {
     private Employee employee;
-    private static Logger logger = Logger.getLogger(EmployeeDaoImpl.class.getName());
+    private static Logger logger = Logger.getLogger(EmployeeDAOImpl.class.getName());
 
     public Employee selectByAuthorization(String ulogin, String upassword) {
-       /* boolean status = false;
+        /* boolean status = false;
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
@@ -58,7 +61,42 @@ public class EmployeeDaoImpl implements DaoGeneric<Employee> {
                     logger.error(e);
                 }
             }
-        }*/
+        } */
         return employee;
+    }
+
+    @Override
+    public Employee getEmployeeByFullName(String name, String surname) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllEnglishTeachers() {
+        return null;
+    }
+
+    @Override
+    public Employee getByID(String ID) {
+        return null;
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public boolean save(Employee employee) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return false;
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return null;
     }
 }
