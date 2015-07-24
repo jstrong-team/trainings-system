964e533b607a85340aeb1154fe8a38fd2a13b3d9
Dzmitry Niadzelka <week62@gmail.com>
2015-07-24 16:12:39 +0300

    fix event bug on calendar

--- forTrainings/web/src/main/webapp/res/calendar/js/calendarController.js
+++ forTrainings/web/src/main/webapp/res/calendar/js/calendarController.js
@@ -1,29 +1,25 @@
 angular.module('calendarModule').controller('calendarController', ['$scope', 'threeMonthList', '$modal', function ($scope, threeMonthList, $modal) {
     $scope.days = getThreeMonthDays();
     $scope.months = getThreeMoths();
-    $scope.showNavbarHat = true;
-    $scope.bla = '----------------------------------';
-    //$scope.isNotLoginPage();
     $scope.$on('ngRepeatFinished', function () {
         markCurrentDay();
     });
 
     $scope.openModal = function (data) {
-        console.log($scope.bla);
         $modal.open({
             animation: true,
             templateUrl: '/res/calendar/modal.html',
             controller: 'calendarModalCtrl',
             size: 'md',
             windowClass: 'modalMain',
-            //backdropClass:'modalBackground',
             resolve: {
                 trainingsStr: function () {
                     var dateRe = /[0-9]{4}-[0-9]{2}-[0-9]{2}/ig;
                     var date = dateRe.exec(data);
+                    console.log(date);
                     return {
-                        date:date[0],
-                        threeMonthTrainings:$scope.threeMonthTrainings
+                        date: date[0],
+                        threeMonthTrainings: $scope.threeMonthTrainings
                     };
                 }
             }
@@ -32,7 +28,6 @@ angular.module('calendarModule').controller('calendarController', ['$scope', 'th
 
     threeMonthList.getThreeMonthList().then(function (data) {
         $scope.threeMonthTrainings = data;
-        $scope.showNavbarHat = false;
         colorDayItems(data);
     });
 }]);
--- forTrainings/web/src/main/webapp/res/calendar/js/onClickDirective.js
+++ forTrainings/web/src/main/webapp/res/calendar/js/onClickDirective.js
@@ -2,13 +2,21 @@ angular.module('calendarModule').directive('onClickDirective', function () {
     return function (scope, element) {
         element.on('click', function (event) {
             var target = event.target;
+
             while (target !== element) {
-                if (target.tagName === 'DIV') {
+
+                if (target.classList[0] === 'dayItem') {
                     var className = target.classList.toString();
-                    //alert(className);
                     scope.openModal(className);
-                    colorDayItems();
+                    event.stopPropagation();
+                    break;
                 }
+
+                if (target.tagName !== 'P') {
+                    event.stopPropagation();
+                    break;
+                }
+
                 target = target.parentNode;
             }
         });
