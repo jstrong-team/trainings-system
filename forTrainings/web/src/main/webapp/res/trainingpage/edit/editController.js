7155dea4d91b306f4b5517cbfd555271f3220a1d
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-07-29 18:07:39 +0300

    edit page fill plus redirect

--- forTrainings/web/src/main/webapp/res/trainingpage/edit/editController.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/edit/editController.js
@@ -1,9 +1,30 @@
 angular.module('trainingEditModule').controller('editController', ['$scope', '$http', 'getTrainingInfo', function ($scope, $http, getTrainingInfo) {
-    getTrainingInfo().then(function(data, status, headers, config){
-        console.log('succes');
-        console.log(data);
-        $scope.editInfo=data.data;
-    },function(error){
+    getTrainingInfo().then(function (data, status, headers, config) {
+        $scope.editInfo = data.data;
+    }, function (error) {
         console.error(error);
     });
+
+    $scope.isCollapsed = true;
+
+    $scope.addDate = function (index) {
+        $scope.editInfo.dates.splice(index,0,'');
+    };
+
+    $scope.removeDate = function (index) {
+        $scope.editInfo.dates.splice(index,1);
+        console.log(index);
+    };
+
+    $scope.datesChange = function (index, value) {
+        $scope.editInfo.dates[index] = value;
+    };
+
+    $scope.applyCahnges = function () {
+        console.log($scope.editInfo);
+    };
+
+
+
+
 }]);
