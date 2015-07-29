399566833f15e1c1a0c7e27b24ed2aaec61f32e4
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-07-29 12:10:17 +0300

    adding unsubscribe

--- forTrainings/web/src/main/webapp/res/trainingpage/edit/module.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/edit/module.js
@@ -1,6 +1,29 @@
 angular.module('trainingEditModule', []).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/ui/trainingPage/edit/:trainingId', {
         templateUrl: 'res/trainingpage/edit/edit.html',
-        controller: 'editController'
+        controller: 'editController',
+        resolve: {
+            getTrainingInfo: ['$http', '$q','$routeParams','$location','$route', 'getRole','getTrainingData',function ($http, $q,$routeParams,$location,$route,getRole,getTrainingData) {
+                var def = $q.defer();
+                var id=$route.current.params.trainingId;
+                getRole(id).then(function (data, status, headers, config){
+                    if (data.data.role === 'trainer') {
+                        getTrainingData(id).then(function (data, status, headers, config) {
+                            console.log(data);
+                            def.resolve(data);
+                        }, function (error) {
+                            console.error(error);
+                        });
+                    } else {
+                        $location.url('/ui/trainings');
+                    }
+                }, function (error) {
+                    console.error(error);
+                });
+                return function () {
+                    return def.promise;
+                };
+            }]
+        }
     });
 }]);
---
+++ forTrainings/web/src/main/webapp/res/trainingpage/services/unsubscribeService.js
@@ -0,0 +1,6 @@
+angular.module('trainingPageModule').factory('unsubscribeService', ['$http', function($http) {
+    var unsubscribe = function(id) {
+        return $http.delete('rest/storagetraining/removeSubscriber?id=' + id);
+    };
+    return unsubscribe;
+}]);
