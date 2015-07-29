3b49991751a86aa33c0dfdf4c4310b10b18deac6
Stas777 <stas@MacBook-Air-Stas.local>
2015-07-29 08:41:47 +0300

    Added editTraining method in TrainingDAO
    Added editTraining method in TrainingDAO

--- forTrainings/core/src/test/java/dao/TrainingDAOTest.java
+++ forTrainings/core/src/test/java/dao/TrainingDAOTest.java
@@ -1,4 +1,37 @@
 package dao;
 
-public class TrainingDAOTest {
+import com.exadel.jstrong.fortrainings.core.dao.TrainingDAO;
+import com.exadel.jstrong.fortrainings.core.model.Training;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TrainingDAOTest extends BaseDAOTest {
+
+    public static final String INITIAL_DATA_LOCATION = "/testdata/initial/training";
+    public static final String EXPECTED_DATA_LOCATION = "/testdata/expected/training";
+
+    @Autowired
+    private TrainingDAO trainingDAO;
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/edit.xml")
+    @ExpectedDatabase(value = EXPECTED_DATA_LOCATION + "/edit.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void testEditTraining() {
+        Training training = new Training();
+        training.setId(1);
+        training.setTrainer_id(1);
+        training.setName("trName");
+        training.setAnnotation("trAnnotation");
+        training.setDescription("trDescription");
+        training.setTarget("trTarget");
+        training.setPaid(true);
+        training.setInternal(true);
+        training.setApprove(true);
+        training.setMax_participants(30);
+
+        trainingDAO.editTraining(training);
+    }
 }
