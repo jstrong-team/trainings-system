b3fd8542a911f49da9c6c1f5483b73f130efacc8
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-07-31 18:49:45 +0300

    absent prefinal version

--- forTrainings/web/src/main/webapp/res/trainingpage/services/onclikCatchDirective.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/services/onclikCatchDirective.js
@@ -1,4 +1,4 @@
-angular.module('trainingPageTrainerModule').directive('tableCatchDirective', ['storageService',function (storageService) {
+angular.module('trainingPageTrainerModule').directive('tableCatchDirective', ['storageService','$modal',function (storageService,$modal) {
      function link(scope, element,attrs){
          element.on('click', function (event) {
              var list=event.target.classList;
@@ -8,10 +8,11 @@ angular.module('trainingPageTrainerModule').directive('tableCatchDirective', ['s
                      id:list[0],
                      date:list[1]+' '+list[2],
                      isAbsent:true,
-                     reason:'why'
+                     reason:''
                  };
                  element.text('No');
                  storageService.set(clickObj);
+                 console.log(storageService.get());
              }else {
                  clickObj={
                      id:list[0],
@@ -21,8 +22,48 @@ angular.module('trainingPageTrainerModule').directive('tableCatchDirective', ['s
                  };
                  element.text('');
                  storageService.delete(clickObj);
+                 console.log(storageService.get());
              }
          });
+
+         element.on('dblclick', function (event) {
+             var list=event.target.classList;
+             var clickObj;
+             if(element.text()!=='No'){
+                 clickObj={
+                     id:list[0],
+                     date:list[1]+' '+list[2],
+                     isAbsent:true,
+                     reason:''
+                 };
+                 var modalInstance =$modal.open({
+                     animation: true,
+                     templateUrl: '/res/trainingpage/reasonModal.html',
+                     controller: 'absentReasonModalCtrl',
+                     windowClass: 'modalMain',
+                     size: 'sm'
+                 });
+                 modalInstance.result.then(function(reason){
+                     element.text('No');
+                     clickObj.reason=reason;
+                     storageService.set(clickObj);
+                     console.log(storageService.get());
+                 });
+             }else {
+                 clickObj={
+                     id:list[0],
+                     date:list[1]+' '+list[2],
+                     isAbsent:false,
+                     reason:''
+                 };
+                 element.text('');
+                 console.log(storageService.get());
+                 storageService.delete(clickObj);
+             }
+         });
+
+
+
      }
     return {
         restrict: 'A',
--- forTrainings/web/src/main/webapp/res/trainingpage/services/storageService.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/services/storageService.js
@@ -1,21 +1,23 @@
 angular.module('trainingPageModule').factory('storageService', ['$http', function ($http) {
     var array=[];
-    var set=function(click){
+
+    var service={};
+
+    service.set=function(click){
         array.push(click);
     };
-    var get=function(){
+    service.get=function(){
         return angular.copy(array);
     };
-    var deleteRecord=function(click){
+    service.delete=function(click){
         for(var i=array.length-1;i>=0;i--){
             if((array[i].id===click.id)&&(array[i].date===click.date)){
                 array.splice(i,1);
             }
         }
     };
-    return {
-        get:get,
-        set:set,
-        delete:deleteRecord
+    service.clear=function(){
+        array=[];
     };
+    return service;
 }]);
