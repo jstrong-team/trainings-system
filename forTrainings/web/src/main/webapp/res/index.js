20b151e2869d49af4a4f1d298b2854a83c4e944f
Dzmitry Niadzelka <week62@gmail.com>
2015-07-31 17:08:36 +0300

    Add pagination to admin news

--- forTrainings/web/src/main/webapp/res/index.js
+++ forTrainings/web/src/main/webapp/res/index.js
@@ -1,8 +1,8 @@
-var moduleApp = angular.module('app', ['ngRoute','ui.bootstrap', 'calendarModule', 'loginModule',
+var moduleApp = angular.module('app', ['ngRoute', 'ui.bootstrap', 'calendarModule', 'loginModule',
     'navigationModule', 'createModule', 'trainingPageModule', 'creationResponseModule',
-    'ui.bootstrap.datetimepicker','trainingPageTrainerModule', 'adminNewsModule','trainingEditModule',
-    'ngLoadingSpinner','trainingPageAdminModule', 'reportsModule']);
+    'ui.bootstrap.datetimepicker', 'trainingPageTrainerModule', 'adminNewsModule', 'trainingEditModule',
+    'ngLoadingSpinner', 'trainingPageAdminModule', 'reportsModule']);
 
-moduleApp.config(['$locationProvider', function($locationProvider) {
+moduleApp.config(['$locationProvider', function ($locationProvider) {
     $locationProvider.html5Mode(true);
 }]);
--- forTrainings/web/src/main/webapp/res/news/adminNews/controller.js
+++ forTrainings/web/src/main/webapp/res/news/adminNews/controller.js
@@ -8,23 +8,52 @@
     var controller = function ($scope, $http) {
 
 
-        var CURRENT_ITEMS_COUNT = 20;
+        var CURRENT_ITEMS_COUNT = 10;
 
 
-        $scope.currentListItems = null;
-        $scope.bigTotalItems = null;
-        $scope.bigCurrentPage = 1;
+        //$scope.actualCurrentListItems = null;
+        //
+        //$scope.historyCurrentListItems = null;
+        //$scope.bigTotalItems = null;
+
+        $scope.currentPage = 1;
         $scope.maxSize = 10;
 
-        $http.get('/rest/news/notice?count=' + CURRENT_ITEMS_COUNT).then(
-            function(data){
-                $scope.bigTotalItems = data;
-                console.log(data);
-            },
-            function(data, status){
-                console.log(status);
-            });
+        //$http.get('/rest/news/notice?count=' + CURRENT_ITEMS_COUNT).then(
+        //    function(data){
+        //        $scope.actualCurrentListItems = data.data.actualNotices;
+        //
+        //        $scope.totalItems = data.data.historyCount;
+        //        $scope.historyCurrentListItems = data.data.historyNotices;
+        //    },
+        //    function(data, status){
+        //        console.log(status);
+        //    });
 
+        $scope.$watch('currentPage', function(newPage){
+            $http.get('/rest/news/noticeHistory?count=' + CURRENT_ITEMS_COUNT + '&page=' + newPage).then(
+                function(data){
+                    $scope.actualCurrentListItems = data.data.actualNotices;
+
+                    $scope.totalItems = data.data.historyCount;
+                    $scope.historyCurrentListItems = data.data.historyNotices;
+                },
+                function(data, status){
+                    console.log(status);
+                });
+        });
+
+        $scope.removeActualItem = function (id) {
+            console.log(id);
+            $http.post('/rest/news/complete', {id: id}).then(
+                function(data, status){
+                    console.log(data);
+                    console.log(status);
+                },
+                function(data, status){
+                    console.log(status);
+                });
+        };
     };
 
     controller.$inject = services;
