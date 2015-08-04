e6ff2384876e363485f32f0749186fce2bc459d3
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-08-04 13:52:04 +0300

    adding edit

--- forTrainings/web/src/main/webapp/res/trainingpage/edit/editService.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/edit/editService.js
@@ -1,9 +1,13 @@
 angular.module('trainingEditModule').factory('editService', ['$http', function($http) {
     var edit =function(id, editInfo) {
+        console.log(editInfo);
+        editInfo.date=new Array(editInfo.meets.length);
+        for(var i=0;i<editInfo.meets.length;i++){
+            editInfo.date[i]=editInfo.meets[i].date;
+        }
         $http.put('/rest/storagetraining/editTraining?id='+id, editInfo).catch(function(error){
             console.error(error);
         });
-        //console.log(editInfo);
     };
     return edit;
 }]);
