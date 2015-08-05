7a3240e8aec81d4675917148082bf3403113af98
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-08-05 10:42:28 +0300

    finish template

--- forTrainings/web/src/main/webapp/res/trainingpage/approvepage/module.js
+++ forTrainings/web/src/main/webapp/res/trainingpage/approvepage/module.js
@@ -1,6 +1,28 @@
-angular.module('approvePageModule',[]).config(['$routeProvider', function ($routeProvider) {
+angular.module('approvePageModule', []).config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/ui/trainingPage/approve/:trainingID', {
         templateUrl: 'res/trainingpage/approvepage/editApprove.html',
-        controller: 'editApproveController'
+        controller: 'editApproveController',
+        resolve: {
+            getApproveData: ['$http', '$q', '$routeParams', '$location', '$route', 'getRole', 'getTrainingData', function ($http, $q, $routeParams, $location, $route, getRole, getTrainingData) {
+                var def = $q.defer();
+                var id = $route.current.params.trainingId;
+                getRole(id).then(function (data, status, headers, config) {
+                    if (data.data.role === 'admin') {
+                        getTrainingData(id).then(function (data, status, headers, config) {
+                            def.resolve(data);
+                        }, function (error) {
+                            console.error(error);
+                        });
+                    } else {
+                        $location.url('/ui/trainings');
+                    }
+                }, function (error) {
+                    console.error(error);
+                });
+                return function () {
+                    return def.promise;
+                };
+            }]
+        }
     });
 }]);
