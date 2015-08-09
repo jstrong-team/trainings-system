5ee91c8911c6d3c29af77200b14e08039a904cd5
MaksimSkavysh <maksim.skavysh@gmail.com>
2015-08-09 15:39:13 +0300

    finish show more

--- forTrainings/web/src/main/webapp/res/calendar/js/calendarModalCtrl.js
+++ forTrainings/web/src/main/webapp/res/calendar/js/calendarModalCtrl.js
@@ -1,8 +1,9 @@
 angular.module('calendarModule').controller('calendarModalCtrl', ['$scope', '$modalInstance', 'trainingsStr', 'trainingRedirectService', function ($scope, $modalInstance, trainingsStr, trainingRedirectService) {
 
-    $scope.date = trainingsStr.date;
-    $scope.trainings = trainingsStr.threeMonthTrainings;
-        $scope.modalSubmit = function () {
+    $scope.trainings = trainingsStr.dayTrainings;
+
+
+    $scope.modalSubmit = function () {
     };
 
     $scope.redirectToTrainingPage = function (id) {
