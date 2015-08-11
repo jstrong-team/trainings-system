de3b5f8664e28be35ebc5328e35b39848a5f1536
Stas777 <stas@MacBook-Air-Stas.local>
2015-08-11 08:05:23 +0300

    Added some tests for EmployeeDAO
    Added some tests for EmployeeDAO

--- forTrainings/core/src/test/java/dao/EmployeeDAOTest.java
+++ forTrainings/core/src/test/java/dao/EmployeeDAOTest.java
@@ -3,6 +3,8 @@ package dao;
 import com.exadel.jstrong.fortrainings.core.dao.EmployeeDAO;
 import com.exadel.jstrong.fortrainings.core.model.Employee;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -15,7 +17,7 @@ import java.util.List;
 public class EmployeeDAOTest extends BaseDAOTest {
 
     public static final String INITIAL_DATA_LOCATION = "/testdata/initial/employee";
-    public static final String EXPECTED_DATA_LOCATION = "/testdata/expected/emploee";
+    public static final String EXPECTED_DATA_LOCATION = "/testdata/expected/employee";
 
     @Autowired
     private EmployeeDAO employeeDAO;
@@ -59,9 +61,107 @@ public class EmployeeDAOTest extends BaseDAOTest {
         expectedList.add(stas);
         expectedList.add(kolya);
 
-        List<Employee> actualList = new LinkedList<>();
-        actualList = employeeDAO.getAllUsers();
+        List<Employee> actualList = employeeDAO.getAllUsers();
 
         Assert.assertEquals(expectedList, actualList);
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/get.xml")
+    public void testGetEmail() {
+        String expectedMail = new String("mariagracova96@gmail.com");
+        String actualMail = employeeDAO.getEmail(2);
+
+        Assert.assertEquals(expectedMail, actualMail);
+    }
+
+    @Test
+    public void testGetEmployeesMails() {
+        List<String> expectedMails = new LinkedList<>();
+        expectedMails.add("mariagracova96@gmail.com");
+        expectedMails.add("staspetrovichbsu@gmail.com");
+        expectedMails.add("nickylebedev@gmail.com");
+
+        List<Employee> employees = new LinkedList<>();
+        Employee masha = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+        Employee stas = new Employee(3, "stas", "c211c9e7e7689217c0cb99aafe30f3d7", "Stas Petrovich", "staspetrovichbsu@gmail.com", null, null);
+        Employee kolya = new Employee(4, "kolya", "ec3da25081aa81b637fe6faa3debe26e", "Nikolay Lebedev", "nickylebedev@gmail.com", null, null);
+        employees.add(masha);
+        employees.add(stas);
+        employees.add(kolya);
+
+        List<String> actualMails = employeeDAO.getEmployeesMails(employees);
+
+        Assert.assertEquals(expectedMails, actualMails);
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/get.xml")
+    public void testGetAdmins() {
+        List<Employee> expectedEmployees = new LinkedList<>();
+        Employee masha = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+        Employee stas = new Employee(3, "stas", "c211c9e7e7689217c0cb99aafe30f3d7", "Stas Petrovich", "staspetrovichbsu@gmail.com", null, null);
+        expectedEmployees.add(masha);
+        expectedEmployees.add(stas);
+
+        List<Employee> actualEmployees = employeeDAO.getAdmins();
+        Assert.assertEquals(expectedEmployees, actualEmployees);
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/get.xml")
+    public void testGetById() {
+        Employee expectedEmployee = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+        Employee actualEmployee = employeeDAO.getById(2);
+
+        Assert.assertEquals(expectedEmployee, actualEmployee);
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/save.xml")
+    @ExpectedDatabase(value = EXPECTED_DATA_LOCATION + "/save.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void testSaveEmployee() {
+        Employee expectedEmployee = new Employee();
+        expectedEmployee.setLogin("dima");
+        expectedEmployee.setPassword("dima");
+        expectedEmployee.setName("Dmitriy Nedelko");
+
+        Employee actualEmployee = employeeDAO.saveEmployee(expectedEmployee);
+
+        Assert.assertEquals(expectedEmployee.getLogin(), actualEmployee.getLogin());
+        Assert.assertEquals("0f5b25cd58319cde0e6e33715b66db4c", actualEmployee.getPassword());
+        Assert.assertEquals(expectedEmployee.getName(), actualEmployee.getName());
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/role.xml")
+    @ExpectedDatabase(value = EXPECTED_DATA_LOCATION + "/role.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+    public void testSetEmployeeRole() {
+        Employee employee = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+
+        employeeDAO.setEmployeeRole(employee, "admin");
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/external.xml")
+    public void testGetAllInsideUsers() {
+        List<Employee> expectedEmployees = new LinkedList<>();
+        Employee masha = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+        Employee stas = new Employee(3, "stas", "c211c9e7e7689217c0cb99aafe30f3d7", "Stas Petrovich", "staspetrovichbsu@gmail.com", null, null);
+        expectedEmployees.add(masha);
+        expectedEmployees.add(stas);
+
+        List<Employee> actualEmployees = new LinkedList<>();
+        actualEmployees = employeeDAO.getAllInsideUsers();
+
+        Assert.assertEquals(expectedEmployees, actualEmployees);
+    }
+
+    @Test
+    @DatabaseSetup(INITIAL_DATA_LOCATION + "/get.xml")
+    public void testGetEmployee() {
+        Employee expectedEmployee = new Employee(2, "masha", "c3cc6e312d2bad42cf535aac3a259abd", "Maria Grakova", "mariagracova96@gmail.com", null, null);
+        Employee actualEmployee = employeeDAO.getEmployee(2);
+        Assert.assertEquals(expectedEmployee, actualEmployee);
     }
 }
